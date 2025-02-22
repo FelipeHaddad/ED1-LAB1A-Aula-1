0799d0b2e63b9731465c48637d5655799b043553
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-02-22 01:04:28 +0000

    ex3 e 4

---
+++ Aula1/Exercicio3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o 1o numero: ");
+        int menorNum = scanner.nextInt();
+        int maiorNum = menorNum;
+
+        for (int i = 1; i < 10; i++){
+            System.out.print("Digite o " + (i + 1) + "numero: ");
+            int num = scanner.nextInt();
+            if (num < menorNum) {
+                menorNum = num;
+            }
+            if (num > maiorNum) {
+                maiorNum = num;
+            }
+        }
+        System.out.println("O menor numero é: " + menorNum);
+        System.out.println("O maior numero é: " + maiorNum);
+
+        scanner.close();
+    }
+}
---
+++ Aula1/Exercicio4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicio4 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] lista = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite o " + (i + 1) + " numero: ");
+            lista[i] = scanner.nextInt();
+            if (i == 9){
+                for (int j = 9; j >= 0; j--) {
+                    System.out.print(lista[j] + " ");
+                }
+            }
+        }
+    }
+}
