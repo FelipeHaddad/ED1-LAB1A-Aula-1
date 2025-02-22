c6ea559eb769be3116ee5026b72f91b5173bf9a9
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-02-22 16:16:42 +0000

    ex5 e 6

---
+++ Aula1/Exercicio5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void min (int a,int  b) {
+            if (a < b) {
+                System.out.println("O menor valor entre " + a + " e " + b + " é " + (a + 1));
+            } else {
+                System.out.println("O menor valor entre " + b + " e " + a + " é " + (b + 1));
+            }
+        }
+    public static void max (int a,int  b) {{
+            if (a > b) {
+                System.out.println("O maior valor entre " + b + " e " + a + " é " + (a - 1));
+            } else {
+                System.out.println("O maior valor entre " + a + " e " + b + " é " + (b - 1));
+            }
+        }
+    }
+
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        int n1 = scanner.nextInt();
+        System.out.print("Digite um segundo valor: ");
+        int n2 = scanner.nextInt();
+
+        if (n1 == n2) {
+            System.out.println("Não há valor entre " + n1 + " e " + n2);
+        } else {
+        min (n1, n2);
+        max (n1, n2);
+
+    }
+} }
---
+++ Aula1/Exercicio6.java
@@ -0,0 +1,12 @@
+public class Exercicio6 {
+    public static double mulComAdicao (double a, int b) {
+        float resultado = 0;
+        for (int i = 1; i <= b; i++) {
+            resultado += a;
+        }
+        return resultado;
+    }
+    public static void main (String[] args) {
+        System.out.print(mulComAdicao(24.5, 6));
+    }
+}
